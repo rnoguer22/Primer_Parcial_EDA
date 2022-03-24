e44aa35e2b1ef02c2129cc386a35db8337bed64f
rnoguer22 <rnogugon@myuax.com>
2022-03-24 08:37:06 +0100

    Definimos la clase Libro asi como su constructor

---
+++ Ejercicio_1.py
@@ -0,0 +1,7 @@
+class Libro:
+    #Definimos el constructor
+    def __init__(self, nombre, autor, ISBN, fecha_publicacion):
+        self.nombre = nombre
+        self.autor = autor
+        self. ISBN = ISBN
+        self.fecha_publicacion = fecha_publicacion

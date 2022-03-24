1ed58ab47efe6122f7032c1e07fab361170fbf09
rnoguer22 <rnogugon@myuax.com>
2022-03-24 08:44:12 +0100

    Finalizamos el ejercicio 1

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -1,13 +1,13 @@
 class Libro:
     #Definimos el constructor
-    def __init__(self, nombre, autor, isbn, fecha_publicacion):
-        self.nombre = nombre
+    def __init__(self, titulo, autor, isbn, fecha_publicacion):
+        self.titulo = titulo
         self.autor = autor
         self. isbn = isbn
         self.fecha_publicacion = fecha_publicacion
 
-    def get_nombre(self):
-        return self.nombre
+    def get_titulo(self):
+        return self.titulo
     
     def get_autor(self):
         return self.autor
@@ -17,3 +17,7 @@ class Libro:
     
     def get_fecha_publicacion(self):
         return self.fecha_publicacion
+
+libro = Libro("Harry Potter", "J.K. Rowling", 217328723, "13/09/2001")
+
+print ("El libro que ha seleccionado es {} y su autor es {}.".format(libro.get_titulo(), libro.get_autor()))

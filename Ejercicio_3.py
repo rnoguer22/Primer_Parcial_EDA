bafde67a6c96b81291c67eba91a5dd7968d45468
rnoguer22 <rnogugon@myuax.com>
2022-03-24 08:57:07 +0100

    Definimos la clase Cuenta Bancaria y su constructor

---
+++ Ejercicio_3.py
@@ -0,0 +1,10 @@
+class Cuenta_Bancaria:
+    #Constructor
+    def __init__(self, id_cuenta, nombre, fecha_apertura, num_cuenta, saldo):
+        self.id_cuenta = id_cuenta
+        self.nombre = nombre
+        self.fecha_apertura = fecha_apertura
+        self.num_cuenta = num_cuenta
+        self.saldo = saldo
+
+    

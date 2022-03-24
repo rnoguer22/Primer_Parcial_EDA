103ca9ad2878dbdd79b297d8c264f2c717f36402
rnoguer22 <rnogugon@myuax.com>
2022-03-24 08:50:10 +0100

    Definimos la clase Mamifero y Oviparo

--- Ejercicio_2.py
+++ Ejercicio_2.py
@@ -2,4 +2,11 @@ class Animal:
     def __init__(self, especie):
         self.especie = especie
 
-class Mamifero:
+class Mamifero(Animal):
+    def __init__(self, especie, mamifero):
+        super().__init__(especie)
+        self.mamifero = mamifero
+class Oviparo(Mamifero):
+    def __init__(self, especie, mamifero, oviparo):
+        super().__init__(especie, mamifero)
+        self.oviparo = oviparo

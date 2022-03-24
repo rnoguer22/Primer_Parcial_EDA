e8ea150b4227ce5711afb90698f1bb1ff5615ad5
rnoguer22 <rnogugon@myuax.com>
2022-03-24 08:38:40 +0100

    Añadimos los getter

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -1,7 +1,19 @@
 class Libro:
     #Definimos el constructor
-    def __init__(self, nombre, autor, ISBN, fecha_publicacion):
+    def __init__(self, nombre, autor, isbn, fecha_publicacion):
         self.nombre = nombre
         self.autor = autor
-        self. ISBN = ISBN
+        self. isbn = isbn
         self.fecha_publicacion = fecha_publicacion
+
+    def get_nombre(self):
+        return self.nombre
+    
+    def get_autor(self):
+        return self.autor
+    
+    def get_isbn(self):
+        return self.isbn
+    
+    def get_fecha_publicacion(self):
+        return self.fecha_publicacion
